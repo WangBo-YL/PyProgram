0050bd7cb696b175ca778478805f473b4576e993
WangBo-YL <bwang18@ivytech.edu>
2024-01-24 22:31:21 -0500

    Add two functions to print names with the first letter capitalized.

--- M02 Lab.py
+++ M02 Lab.py
@@ -5,14 +5,19 @@ Discription: A program that checks if a student is on the Dean's List or Honor R
 '''
 
 while True:
-    last_name = input("Please enter your last name: ")
-    if last_name == "ZZZ":
+    # Getting user input and formatting
+    last_name = input("Please enter your last name (enter ZZZ to quit): ").strip().title()
+    if last_name.upper() == "ZZZ":
         break
-    first_name = input("Please enter your first name: ")
+    first_name = input("Please enter your first name: ").strip().title()
+
+    # Getting user's GPA
     GPA = float(input("Please enter your GPA: "))
+
+    # Checking for Dean's List and Honor Roll, and printing accordingly
     if GPA >= 3.5:
-        print(first_name, last_name, "has made the Dean's List.")
+        print(f"{first_name} {last_name} has made the Dean's List.")
     elif GPA >= 3.25:
-        print(first_name, last_name, "has made the Honor Roll.")
+        print(f"{first_name} {last_name} has made the Honor Roll.")
     else:
-        print(first_name, last_name, "has not made the Dean's List or Honor Roll.")
+        print(f"{first_name} {last_name} has not made the Dean's List or Honor Roll.")
