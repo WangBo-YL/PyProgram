0a82269af21736e86b0be6704a62dec795dadd97
SpidaXx <boblueheart@gmail.com>
2024-02-09 11:15:28 -0500

    delete some comments

--- M04/Assignment/zoo.py
+++ M04/Assignment/zoo.py
@@ -1,15 +1,2 @@
 def hours():
     print('Open 9-5 daily')
-
-
-'''
->>> import sys
->>> sys.path.append(r'C:\Users\Bo\Applications\SDEV220\PyProgram\M04')
->>> import zoo
->>> zoo.hours()
-Open 9-5 daily
->>> import zoo as menagerie
->>> menagerie.hours()
-Open 9-5 daily
->>>
-'''
